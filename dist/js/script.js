c78906dd49919cad1e0b48694a52f9b65b029bd3
vasylkucherak <vasil.kucherak@gmail.com>
2022-02-27 15:17:00 +0200

    v.0.1.0 (created menu panel, optimized for all devices and writed and wrote a script for it)

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
+    const menuPanel = document.querySelector('.menu__panel'),
+          menuItems = menuPanel.querySelectorAll('.menu__item');
+    
+    menuPanel.addEventListener('click', (e) => {
+        if (e.target.classList.contains('menu__item')) {
+            menuItems.forEach(menuItem => menuItem.classList.remove('active'));
+            e.target.classList.add('active');
+        }
+    });
 
 });
