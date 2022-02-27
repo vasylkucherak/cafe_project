024c1a9c3e809fc73593138a44aaea868f5ff94f
vasylkucherak <vasil.kucherak@gmail.com>
2022-02-27 17:58:32 +0200

    v.0.1.5 (created menu cards and optimized for all devices)

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
+    let cart = {
+        'lamb': {'count': 0, 'price': 620},
+        'turkey': {'count': 0, 'price': 450},
+        'goose': {'count': 0, 'price': 7900},
+        'duck': {'count': 0, 'price': 3230}
+    };
+
     const menuPanel = document.querySelector('.menu__panel'),
-          menuItems = menuPanel.querySelectorAll('.menu__item');
+          menuItems = menuPanel.querySelectorAll('.menu__item'),
+          cardsWrapper = document.querySelector('.menu__cards'),
+          cards = cardsWrapper.querySelectorAll('.card'),
+          adds = cardsWrapper.querySelectorAll('.add');
     
+    // click on tabs
     menuPanel.addEventListener('click', (e) => {
         if (e.target.classList.contains('menu__item')) {
             menuItems.forEach(menuItem => menuItem.classList.remove('active'));
@@ -10,4 +21,10 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    // click in add on cart 
+    adds.forEach((add, i) => {
+        add.addEventListener('click', () => {
+            cards[i].classList.add('active');
+        });
+    });
 });
